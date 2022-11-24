ca28199d6bdcaa99c7d906d93c819299b323608a
Mevada01 <harsh.b.mevada@gmail.com>
2022-11-24 17:25:26 +0530

    updated

---
+++ assets/js/js1.js
@@ -0,0 +1,16 @@
+// let fbtn = document.querySelector('a');
+// fbtn.addEventListener('click',showMsg);
+
+// function showMsg(){
+//     alert("This Wesite is Under progress")
+// }
+
+function loadweb(){
+    alert("This website is under progress Please Show on 9 December 2022");
+    useEffect(() => {
+        effect
+        return () => {
+            cleanup
+        };
+    }, [input]);
+}
